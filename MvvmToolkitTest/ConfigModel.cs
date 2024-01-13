d3e054aae45151b7fc789bf8b54537dab347e2f6
sosuisen <hidekazu.kubota@gmail.com>
2024-01-13 11:07:51 +0900

    プロジェクト ファイルを追加します。

---
+++ MvvmToolkitTest/ConfigModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvvmPractice
+{
+    class ConfigModel(int volume = 50)
+    {
+        public int Volume { get; set; } = volume;
+
+        public void SaveConfig() => System.Diagnostics.Debug.WriteLine($"SaveConfig: Volume={Volume}");
+    }
+}
